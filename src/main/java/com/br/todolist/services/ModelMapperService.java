117c25ec8ed38a737ee20244bd6d3e49ff226f1d
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-10-13 14:48:02 -0300

    Creation of  ModellMapper Bean.

---
+++ src/main/java/com/br/todolist/services/ModelMapperService.java
@@ -0,0 +1,14 @@
+package com.br.todolist.services;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ModelMapperService {
+    
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
+    }
+}
