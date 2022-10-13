7c14f632e0afdaeaa251aff5691c223e35402372
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-10-13 20:42:19 -0300

    Addition of relation.

--- src/main/java/com/br/todolist/models/TaskDataModel.java
+++ src/main/java/com/br/todolist/models/TaskDataModel.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.br.todolist.enums.LevelOfImportance;
@@ -35,5 +37,9 @@ public class TaskDataModel {
     private LevelOfImportance importance;
     private String addressTask;
     private LocalDateTime dateTask;
-    private LocalDateTime dateCreateTask; 
+    private LocalDateTime dateCreateTask;
+    
+    @ManyToOne
+    @JoinColumn(name="people_id")
+    private PeopleModel people;
 }
