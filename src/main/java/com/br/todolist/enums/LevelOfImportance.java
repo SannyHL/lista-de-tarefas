33b8059a396b09667cf718773a17ef05b6b82854
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-10-16 21:53:41 -0300

    Insertion of values.

--- src/main/java/com/br/todolist/enums/LevelOfImportance.java
+++ src/main/java/com/br/todolist/enums/LevelOfImportance.java
@@ -1,5 +1,12 @@
 package com.br.todolist.enums;
 
 public enum LevelOfImportance {
-    
+    LEVE("Leve"),
+    MEDIO("Médio"),
+    IMPORTANTE("Importante"),
+    MUITOIMPORTANTE("Muito Importante"),
+    URGENTE("Urgente");
+
+    LevelOfImportance(String levelImportance){
+    }
 }
