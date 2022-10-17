4bcce36c9dc8df216e9977d127b9c8df1d5578c0
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-10-16 21:55:40 -0300

    Refactoring user.

--- src/main/java/com/br/todolist/config/SecurityConfig.java
+++ src/main/java/com/br/todolist/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth.inMemoryAuthentication()
-        .withUser("api")
+        .withUser("apilist")
         .password("{noop}123")
         .roles("roles");
     }
