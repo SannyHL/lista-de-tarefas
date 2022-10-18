cb3d68b1dafe43658b3816599593f1ce378c2330
Sanny Helen Lima <sannyhelenlima@gmail.com>
2022-10-18 12:23:29 -0300

    Insertion findById and create.

--- src/main/java/com/br/todolist/controllers/PeopleController.java
+++ src/main/java/com/br/todolist/controllers/PeopleController.java
@@ -1,16 +1,23 @@
 package com.br.todolist.controllers;
 
+import java.net.URI;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.br.todolist.dtos.PeopleDTO;
+import com.br.todolist.models.PeopleModel;
 import com.br.todolist.services.PeopleService;
 
 import io.swagger.annotations.Api;
@@ -21,15 +28,33 @@ import io.swagger.annotations.ApiOperation;
 @Api(tags = "API Controller")
 public class PeopleController {
     
+    /**
+     *
+     */
+    private static final String ID = "/{id}";
     @Autowired
     private PeopleService service;
     @Autowired
     private ModelMapper mapper;
 
     @GetMapping
-    @ApiOperation("Listen all")
+    @ApiOperation("List all")
     public ResponseEntity<List<PeopleDTO>> findAll(){
         return ResponseEntity.ok().body(service.findAll().stream().map(x -> mapper.map(x, PeopleDTO.class)).collect(Collectors.toList()));
+    }
+
+    @GetMapping(ID)
+    @ApiOperation("Find by id")
+    public ResponseEntity<PeopleDTO> findById(@PathVariable UUID id) throws Exception{
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), PeopleDTO.class));
+    }
+
+    @PostMapping
+    @ApiOperation("Create")
+    public ResponseEntity<PeopleDTO> create(@RequestBody PeopleDTO peopleDTO){
+        PeopleModel peopleModel = service.create(peopleDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path(ID).buildAndExpand(peopleModel.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 }
